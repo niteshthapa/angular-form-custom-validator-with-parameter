eb3000b0eaca7718ae818c2d81d7742935235fa0
Nitesh <nitesh_thapa@ymail.com>
2021-08-04 01:03:44 +0530

    angular-form-custom-validator-with-parameter

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, AbstractControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  isFormSubmit: boolean = false;
+  validateForm = new FormGroup({
+    country: new FormControl('Select', this.DropdownSelectValidation("Afghanistan"))
+  })
+  get country() { return this.validateForm.get('country'); }
+  DropdownSelectValidation(validationValue: string) {
+    return (control: AbstractControl) => {
+      console.log(control)
+      if (control.value === validationValue) {
+        return { 'required': true }
+      }
+      else {
+        return null;
+      }
+    }
+  }
+  onSubmit() {
+    this.isFormSubmit = true;
+    if (!this.validateForm.valid) {
+      console.log("error");
+    }
+    else {
+      console.log(this.validateForm.value)
+    }
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
